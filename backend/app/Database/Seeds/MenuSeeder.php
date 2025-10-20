60f7423221f1ded524897afe37b9537b3cfcc1aa
KicaiBadai <adityamaula80@gmail.com>
2025-10-20 14:14:35 +0700

    Menu

--- backend/app/Database/Seeds/MenuSeeder.php
+++ backend/app/Database/Seeds/MenuSeeder.php
@@ -12,56 +12,267 @@ class MenuSeeder extends Seeder
         $data = [
             // Level 1 (Menu Utama: parent_id = 0)
             [
-                'menu_name' => 'Beranda', 
-                'menu_url' => '/dashboard', 
+                'menu_name' => 'PROFILE', 
+                'menu_url' => '#', 
                 'menu_icon' => 'fa-home', 
                 'order_number' => 1, 
                 'parent_id' => 0
             ],
             [
-                'menu_name' => 'Master Data', 
+                'menu_name' => 'BERITA', 
                 'menu_url' => '#', 
                 'menu_icon' => 'fa-database', 
                 'order_number' => 2, 
                 'parent_id' => 0
             ],
             [
-                'menu_name' => 'Laporan', 
-                'menu_url' => '/reports', 
+                'menu_name' => 'PROGRAM', 
+                'menu_url' => '#', 
                 'menu_icon' => 'fa-chart', 
                 'order_number' => 3, 
                 'parent_id' => 0
             ],
+            [
+                'menu_name' => 'INFORMASI PUBLIK', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-database', 
+                'order_number' => 4, 
+                'parent_id' => 0
+            ],
+            [
+                'menu_name' => 'PPID', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-database', 
+                'order_number' => 5, 
+                'parent_id' => 0
+            ],
+            [
+                'menu_name' => 'GALERI', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-database', 
+                'order_number' => 6, 
+                'parent_id' => 0
+            ],
+            [
+                'menu_name' => 'KONTAK', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-database', 
+                'order_number' => 7, 
+                'parent_id' => 0
+            ],
             
             // Level 2 (Sub-Menu dari Master Data: parent_id = 2)
+            // level 2 Untuk Profile
             [
-                'menu_name' => 'Pengguna', 
-                'menu_url' => '/master/users', 
+                'menu_name' => 'Tentang', 
+                'menu_url' => '#', 
                 'menu_icon' => 'fa-users', 
                 'order_number' => 1, 
                 'parent_id' => 2
             ],
             [
-                'menu_name' => 'Produk', 
-                'menu_url' => '/master/products', 
-                'menu_icon' => 'fa-box', 
-                'order_number' => 2, 
+                'menu_name' => 'Profil Pejabat Struktural', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-users', 
+                'order_number' => 1, 
+                'parent_id' => 2
+            ],
+            [
+                'menu_name' => 'Tugas Dan Fungsi', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-users', 
+                'order_number' => 1, 
+                'parent_id' => 2
+            ],
+            [
+                'menu_name' => 'Struktur Organisasi', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-users', 
+                'order_number' => 1, 
+                'parent_id' => 2
+            ],
+            [
+                'menu_name' => 'Seketariat', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-users', 
+                'order_number' => 1, 
+                'parent_id' => 2
+            ],
+            [
+                'menu_name' => 'Bidang Diseminasi Informasi Dan Komunikasi Publik', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-users', 
+                'order_number' => 1, 
+                'parent_id' => 2
+            ],
+            [
+                'menu_name' => 'Bidang Saran, Prasarana TIK dan Persandian', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-users', 
+                'order_number' => 1, 
+                'parent_id' => 2
+            ],
+            [
+                'menu_name' => 'Bidang Statistika Dan Pemberdayaan TIK', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-users', 
+                'order_number' => 1, 
+                'parent_id' => 2
+            ],
+            [
+                'menu_name' => 'Bidang Pengembangan E-Goverment', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-users', 
+                'order_number' => 1, 
                 'parent_id' => 2
             ],
             
-            // Level 3 (Sub-Menu dari Produk: parent_id = 5)
+            // level 2 Untuk Program
             [
-                'menu_name' => 'Kategori Produk', 
-                'menu_url' => '/master/products/categories', 
-                'menu_icon' => 'fa-tags', 
-                'order_number' => 1, 
+                'menu_name' => 'Program SKPD', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-box', 
+                'order_number' => 3, 
+                'parent_id' => 2
+            ],
+
+            // level 2 Untuk Informasi Publik
+            [
+                'menu_name' => 'Perencanaan', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-box', 
+                'order_number' => 4, 
+                'parent_id' => 2
+            ],
+            [
+                'menu_name' => 'Pengadaan Barang dan Jasa', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-box', 
+                'order_number' => 4, 
+                'parent_id' => 2
+            ],
+            [
+                'menu_name' => 'Laporan Keuangan', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-box', 
+                'order_number' => 4, 
+                'parent_id' => 2
+            ],
+            [
+                'menu_name' => 'Laporan Kinerja', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-box', 
+                'order_number' => 4, 
+                'parent_id' => 2
+            ],
+            [
+                'menu_name' => 'Pendidikan dan Pelatihan', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-box', 
+                'order_number' => 4, 
+                'parent_id' => 2
+            ],
+            [
+                'menu_name' => 'Permohonan Informasi', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-box', 
+                'order_number' => 4, 
+                'parent_id' => 2
+            ],
+            [
+                'menu_name' => 'Daftar Informasi Publik', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-box', 
+                'order_number' => 4, 
+                'parent_id' => 2
+            ],
+            [
+                'menu_name' => 'Kerjasama Daerah', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-box', 
+                'order_number' => 4, 
+                'parent_id' => 2
+            ],
+            [
+                'menu_name' => 'Regulasi', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-box', 
+                'order_number' => 4, 
+                'parent_id' => 2
+            ],
+
+            // lvl 2 untuk ppibd
+            [
+                'menu_name' => 'Ajukan Permohonan', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-box', 
+                'order_number' => 5, 
+                'parent_id' => 2
+            ],
+            [
+                'menu_name' => 'Lacak Permohonan', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-box', 
+                'order_number' => 5, 
+                'parent_id' => 2
+            ],
+            [
+                'menu_name' => 'Keberatan', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-box', 
+                'order_number' => 5, 
+                'parent_id' => 2
+            ],
+            [
+                'menu_name' => 'Klasifikasi Informasi', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-box', 
+                'order_number' => 5, 
+                'parent_id' => 2
+            ],
+
+            // level 2 dari galeri
+            [
+                'menu_name' => 'Galeri Foto', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-box', 
+                'order_number' => 6, 
+                'parent_id' => 2
+            ],
+            [
+                'menu_name' => 'Galeri Video', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-box', 
+                'order_number' => 6, 
+                'parent_id' => 2
+            ],
+            // Level 3 dari PPIBD
+            [
+                'menu_name' => 'Berkala', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-box', 
+                'order_number' => 5, 
                 'parent_id' => 5
             ],
             [
-                'menu_name' => 'Varian Produk', 
-                'menu_url' => '/master/products/variants', 
-                'menu_icon' => 'fa-cubes', 
-                'order_number' => 2, 
+                'menu_name' => 'Setiap Saat', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-box', 
+                'order_number' => 5, 
+                'parent_id' => 5
+            ],
+            [
+                'menu_name' => 'Serta Meta', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-box', 
+                'order_number' => 5, 
+                'parent_id' => 5
+            ],
+            [
+                'menu_name' => 'Dikecualikan', 
+                'menu_url' => '#', 
+                'menu_icon' => 'fa-box', 
+                'order_number' => 5, 
                 'parent_id' => 5
             ],
         ];
